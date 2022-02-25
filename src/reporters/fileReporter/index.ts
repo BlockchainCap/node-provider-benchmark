9746d1148ae66c8a7e603186d173f72f00976476
Ryan Sproule <rsproule23@gmail.com>
2022-02-25 14:33:46 -0800

    handle missing file

--- src/reporters/fileReporter/index.ts
+++ src/reporters/fileReporter/index.ts
@@ -3,8 +3,10 @@ import * as fs from 'fs'
 
 export const reportMetric = async (metric: Metric) => {
   if (!fs.existsSync('out/report.txt')) {
-    fs.mkdirSync('out')
+    if (!fs.existsSync('out')) {
+      fs.mkdirSync('out')
+    }
     fs.writeFileSync('out/report.txt', '')
   }
-  fs.appendFileSync('out/report.txt', JSON.stringify(metric))
+  fs.appendFileSync('out/report.txt', JSON.stringify(metric) + '\n')
 }
